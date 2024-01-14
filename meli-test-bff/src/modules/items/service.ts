e04c1426eaaf007f90ca65c372de7d5924a3f958
angelliambo <aliambo@jobint.com>
2024-01-14 19:17:33 -0300

    feat(pageSearch): bff connected [tiket-001]

--- meli-test-bff/src/modules/items/service.ts
+++ meli-test-bff/src/modules/items/service.ts
@@ -1,36 +1,44 @@
-import axios from 'axios';
-import { Request } from 'express';
+import axios from "axios";
+import { Request } from "express";
 
 class ExampleService {
-  async searchItems(req: Request) {
-    try {
-      const response = await axios.get(`${process.env.MELI_API}/sites/MLA/search?q=${req.query.q}&limit=4`);
-      return response.data;
-    } catch (error) {
-      console.error('Error:', error);
-      throw error; // Re-throw the error to handle it in the controller
-    }
-  }
+	async searchItems(req: Request) {
+		console.log(":: ~ ExampleService ~ searchItems ~ req", req.query.q);
 
-  async getItemById(req: Request) {
-    try {
-      const response = await axios.get(`${process.env.MELI_API}/items/${req.params.id}`);
-      return response.data;
-    } catch (error) {
-      console.error('Error:', error);
-      throw error; // Re-throw the error to handle it in the controller
-    }
-  }
+		try {
+			const response = await axios.get(
+				`${process.env.MELI_API}/sites/MLA/search?q=${req.query.q}&limit=4`
+			);
+			return response.data;
+		} catch (error) {
+			console.error("Error:", error);
+			throw error; // Re-throw the error to handle it in the controller
+		}
+	}
 
-  async getItemDescription(req: Request) {
-    try {
-      const response = await axios.get(`${process.env.MELI_API}/items/${req.params.id}/description`);
-      return response.data;
-    } catch (error) {
-      console.error('Error:', error);
-      throw error; // Re-throw the error to handle it in the controller
-    }
-  }
+	async getItemById(req: Request) {
+		try {
+			const response = await axios.get(
+				`${process.env.MELI_API}/items/${req.params.id}`
+			);
+			return response.data;
+		} catch (error) {
+			console.error("Error:", error);
+			throw error; // Re-throw the error to handle it in the controller
+		}
+	}
+
+	async getItemDescription(req: Request) {
+		try {
+			const response = await axios.get(
+				`${process.env.MELI_API}/items/${req.params.id}/description`
+			);
+			return response.data;
+		} catch (error) {
+			console.error("Error:", error);
+			throw error; // Re-throw the error to handle it in the controller
+		}
+	}
 }
 
 export default ExampleService;
