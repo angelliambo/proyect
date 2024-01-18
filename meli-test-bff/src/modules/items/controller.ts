ed3eac369b2d04b67e7dfad88667d2ea1d72b0c7
angelliambo <aliambo@jobint.com>
2024-01-18 14:41:49 -0300

    feat(deuda-tecnica): author [tiket-004]

--- meli-test-bff/src/modules/items/controller.ts
+++ meli-test-bff/src/modules/items/controller.ts
@@ -1,42 +1,54 @@
-import { Request, Response } from 'express';
-import Service from './service';
+import { Request, Response } from "express";
+import Service from "./service";
+
+const editData = (data) => {
+	const modifiedData = {
+		...data,
+		author: {
+			name: "Angel Adrián",
+			lastname: "Liambo",
+		},
+	};
+	return modifiedData;
+};
 
 class Controller {
-  private service: Service;
+	private service: Service;
 
-  constructor() {
-    this.service = new Service();
-  }
+	constructor() {
+		this.service = new Service();
+	}
 
-  async searchItems(req: Request, res: Response) {
-    try {
-      const data = await this.service.searchItems(req);
-      res.json(data);
-    } catch (error) {
-      console.error('Error! searchItems.')
-      res.status(500).json({ error: 'Error al obtener los datos.' });
-    }
-  }
+	async searchItems(req: Request, res: Response) {
+		try {
+			const data = await this.service.searchItems(req);
 
-  async getItemById(req: Request, res: Response) {
-    try {
-      const data = await this.service.getItemById(req);
-      res.json(data);
-    } catch (error) {
-      console.error('Error! getItemById.')
-      res.status(500).json({ error: 'Error al obtener los datos.' });
-    }
-  }
+			res.json(editData(data));
+		} catch (error) {
+			console.error("Error! searchItems.");
+			res.status(500).json({ error: "Error al obtener los datos." });
+		}
+	}
 
-  async getItemDescription(req: Request, res: Response) {
-    try {
-      const data = await this.service.getItemDescription(req);
-      res.json(data);
-    } catch (error) {
-      console.error('Error! getItemDescription.')
-      res.status(500).json({ error: 'Error al obtener los datos.' });
-    }
-  }
+	async getItemById(req: Request, res: Response) {
+		try {
+			const data = await this.service.getItemById(req);
+			res.json(editData(data));
+		} catch (error) {
+			console.error("Error! getItemById.");
+			res.status(500).json({ error: "Error al obtener los datos." });
+		}
+	}
+
+	async getItemDescription(req: Request, res: Response) {
+		try {
+			const data = await this.service.getItemDescription(req);
+			res.json(editData(data));
+		} catch (error) {
+			console.error("Error! getItemDescription.");
+			res.status(500).json({ error: "Error al obtener los datos." });
+		}
+	}
 }
 
 export default Controller;
