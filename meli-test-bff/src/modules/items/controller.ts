7b482967627717337a5b6fe32dfc6cdd3ac8b4fa
angelliambo <aliambo@jobint.com>
2024-01-18 15:02:11 -0300

    feat(deuda-tecnica): dipados y coments bff [tiket-004]

--- meli-test-bff/src/modules/items/controller.ts
+++ meli-test-bff/src/modules/items/controller.ts
@@ -1,6 +1,14 @@
 import { Request, Response } from "express";
 import Service from "./service";
+import { ItemDescriptionDto, ItemDto, itemsDto } from "./dto/item.dto";
 
+/**
+ * editData:
+ *  el param data es la respuesta previamente obtenida de la api de mercado-libre
+ *  antes de enviarla al front, le agregamos firma de autor segun se ha solicitado
+ * @param {*} data
+ * @return {*} data + firma de autor
+ */
 const editData = (data) => {
 	const modifiedData = {
 		...data,
@@ -19,9 +27,18 @@ class Controller {
 		this.service = new Service();
 	}
 
+	/**
+	 * searchItems:
+	 *  Solicitamos a la  api realizar un busqueda general
+	 * req: string required
+	 *
+	 * @param {Request} req
+	 * @param {Response} res
+	 * @memberof Controller
+	 */
 	async searchItems(req: Request, res: Response) {
 		try {
-			const data = await this.service.searchItems(req);
+			const data: itemsDto = await this.service.searchItems(req);
 
 			res.json(editData(data));
 		} catch (error) {
@@ -30,9 +47,18 @@ class Controller {
 		}
 	}
 
+	/**
+	 * getItemById:
+	 *  Solicitamos a la  api realizar un busqueda especifica
+	 * id: string required
+	 *
+	 * @param {Request} req
+	 * @param {Response} res
+	 * @memberof Controller
+	 */
 	async getItemById(req: Request, res: Response) {
 		try {
-			const data = await this.service.getItemById(req);
+			const data: ItemDto = await this.service.getItemById(req);
 			res.json(editData(data));
 		} catch (error) {
 			console.error("Error! getItemById.");
@@ -40,9 +66,21 @@ class Controller {
 		}
 	}
 
+	/**
+	 * getItemDescription:
+	 *  Solicitamos a la  api realizar un busqueda
+	 *  de la descripcion de un elemento en particular
+	 * id: string required
+	 *
+	 * @param {Request} req
+	 * @param {Response} res
+	 * @memberof Controller
+	 */
 	async getItemDescription(req: Request, res: Response) {
 		try {
-			const data = await this.service.getItemDescription(req);
+			const data: ItemDescriptionDto = await this.service.getItemDescription(
+				req
+			);
 			res.json(editData(data));
 		} catch (error) {
 			console.error("Error! getItemDescription.");
